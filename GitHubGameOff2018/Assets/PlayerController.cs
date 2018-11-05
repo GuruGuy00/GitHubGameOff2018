1fcbcf32ea76d33ca28b6201f232b34ed1de9296
Ken Darling <GuruGuy00@Gmail.com>
2018-11-05 15:32:21 -0500

    Movement/Move List
    Rough workings of the movement system

--- GitHubGameOff2018/Assets/PlayerController.cs
+++ GitHubGameOff2018/Assets/PlayerController.cs
@@ -11,19 +11,31 @@ public class PlayerController : MonoBehaviour {
     public Vector3 playerLoc;
     public Vector3 LocTest;
 
+    public Vector3Int newPos;
+
+    private float moveTime = 1f;
+
+    Vector3 currentVelocity;
+    public float smoothTime = 0.25f;
+
+    //ToDo: Get better movements tile by tile
+
     // Use this for initialization
     void Start () {
 
-        LocTest = groundTilemap.CellToWorld(new Vector3Int(5, 5, 0));
-        transform.position = groundTilemap.CellToWorld(new Vector3Int(5, 5, 0));
-        playerLoc = groundTilemap.CellToWorld(new Vector3Int(5, 5, 0));
+        //ToDo : fix this up, need to read start pos from var
+        LocTest = groundTilemap.CellToWorld(new Vector3Int(-8, -4, 0));
+        transform.position = groundTilemap.CellToWorld(new Vector3Int(-8, -4, 0));
+        playerLoc = groundTilemap.CellToWorld(new Vector3Int(-8, -4, 0));
 
+        newPos = Vector3Int.CeilToInt(playerLoc);
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        Vector3Int newPos =  Vector3Int.CeilToInt(playerLoc);
+        //Vector3Int newPos =  Vector3Int.CeilToInt(playerLoc);
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
@@ -39,18 +51,46 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             newPos = new Vector3Int((int)playerLoc.x, (int)playerLoc.y - 1, 0);
-            
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            newPos = new Vector3Int((int)playerLoc.x - 1, (int)playerLoc.y, 0);
+            newPos = new Vector3Int((int)playerLoc.x - 1, (int)playerLoc.y, 0); ;
         }
 
         playerLoc = groundTilemap.CellToWorld(newPos);
-        transform.position = playerLoc;
+        //transform.position = playerLoc;
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref currentVelocity, smoothTime);
 
         Debug.Log(getCell(groundTilemap, playerLoc));
+
+    }
+
+    public void MoveRight()
+    {
+        //newPos = Vector3Int.CeilToInt(playerLoc);
+        newPos = new Vector3Int((int)playerLoc.x + 1, (int)playerLoc.y, 0);
+        playerLoc = groundTilemap.CellToWorld(newPos);
+        //transform.position = playerLoc;
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref currentVelocity, smoothTime);
+    }
+
+    public void MoveLeft()
+    {
+        //Vector3Int newPos = Vector3Int.CeilToInt(playerLoc);
+        newPos = new Vector3Int((int)playerLoc.x - 1, (int)playerLoc.y, 0);
+        playerLoc = groundTilemap.CellToWorld(newPos);
+        //transform.position = playerLoc;
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref currentVelocity, smoothTime);
+    }
+
+    public void MoveUp()
+    {
+        //Vector3Int newPos = Vector3Int.CeilToInt(playerLoc);
+        newPos = new Vector3Int((int)playerLoc.x, (int)playerLoc.y + 1, 0);
+        playerLoc = groundTilemap.CellToWorld(newPos);
+        //transform.position = playerLoc;
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref currentVelocity, smoothTime);
 
     }
 
@@ -58,4 +98,6 @@ public class PlayerController : MonoBehaviour {
     {
         return tilemap.GetTile(tilemap.WorldToCell(cellWorldPos));
     }
+
+    
 }
---
+++ GitHubGameOff2018/Assets/Scripts/Move.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject player; 
+    PlayerController playerController;
+    List<string> moveList = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+       
+        playerController = player.GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddLeftMove()
+    {
+
+        moveList.Add("Left");
+    }
+
+    public void AddRightMove()
+    {
+
+        moveList.Add("Right");
+    }
+
+    public void AddJumpMove()
+    {
+        moveList.Add("Jump");
+
+    }
+
+    public void SubmitMoves()
+    {
+        foreach (string move in moveList)
+        {
+
+            Debug.Log(move);
+            switch (move)
+            {
+                case "Right":
+                    playerController.MoveRight();
+                    break;
+                case "Left":
+                    playerController.MoveLeft();
+                    break;
+                case "Jump":
+                    playerController.MoveUp();
+                    playerController.MoveUp();
+                    playerController.MoveUp();
+                    playerController.MoveRight();
+                    break;
+                default:
+                    break;
+            }
+         
+        }
+
+        moveList.Clear();
+    }
+
+}
