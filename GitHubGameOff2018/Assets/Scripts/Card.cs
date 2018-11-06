ec96c4ed80aaf537a65634525eb912f051acf17d
mwharris <mharris161@gmail.com>
2018-11-06 10:26:37 -0500

    Created a general card design prefab. Created Card and CardDisplay scripts for holding and displaying card data on the prefab. Created a ScriptableObject for the first card: Move Left. Added sprites for card "artwork".

---
+++ GitHubGameOff2018/Assets/Scripts/Card.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject {
+
+    public string cardName;
+    public string description;
+    public Sprite artwork;
+    public int actionCost;
+	
+}
---
+++ GitHubGameOff2018/Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour {
+
+    public Card card;
+
+    public Text nameText;
+    public Text descriptionText;
+    public Text actionText;
+
+    public Image artworkImage;
+
+    void Start ()
+    {
+		if (card != null)
+        {
+            nameText.text = card.cardName;
+            descriptionText.text = card.description;
+            actionText.text = card.actionCost.ToString();
+            artworkImage.sprite = card.artwork;
+        }
+	}
+
+}
