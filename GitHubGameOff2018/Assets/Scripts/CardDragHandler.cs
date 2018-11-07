dfe5e873e22421c46f7afb6b9b070e54e4445a8a
Ken Darling <GuruGuy00@Gmail.com>
2018-11-07 16:35:20 -0500

    Added Click and Drag to the card
    WIP does not do much yet.

---
+++ GitHubGameOff2018/Assets/Scripts/CardDragHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    //ToDo : need much work... 
+    //Ref Code https://forum.unity.com/threads/inventory-drag-and-drop-code.518046/
+
+    private Vector3 startPOS;
+    private void Start()
+    {
+        startPOS = transform.position;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //transform.localPosition = Vector3.zero;
+        transform.position = startPOS;
+    }
+}
