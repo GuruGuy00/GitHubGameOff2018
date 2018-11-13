80edf075ba6b827d16cfc101abcf4d44e781ffd7
Ken Darling <GuruGuy00@Gmail.com>
2018-11-13 14:09:31 -0500

    Update to Drag and Drop
    Need to update some more to handle vertical drop zones.

--- GitHubGameOff2018/Assets/Scripts/CardDropHandler.cs
+++ GitHubGameOff2018/Assets/Scripts/CardDropHandler.cs
@@ -3,8 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CardDropHandler : MonoBehaviour, IDropHandler {
+public class CardDropHandler : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+        {
+            return;
+        }
+
+        Draggable dragHandler = eventData.pointerDrag.GetComponent<Draggable>();
+        if (dragHandler != null)
+        {
+            dragHandler.placeHolderParent = transform;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+        {
+            return;
+        }
+
+        Draggable dragHandler = eventData.pointerDrag.GetComponent<Draggable>();
+        if (dragHandler != null && dragHandler.placeHolderParent == this.transform)
+        {
+            dragHandler.placeHolderParent = dragHandler.parentToReturnTo;
+        }
+    }
 
     public void OnDrop(PointerEventData eventData)
     {
--- GitHubGameOff2018/Assets/Scripts/Draggable.cs
+++ GitHubGameOff2018/Assets/Scripts/Draggable.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
 
     public Transform parentToReturnTo = null;
+    public Transform placeHolderParent = null;
+
+    public GameObject placeHolder = null;
 
     void Start()
     {
@@ -25,7 +29,18 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     {
         Debug.Log("OnBeginDrag");
 
+        placeHolder = new GameObject();
+        placeHolder.transform.SetParent(this.transform.parent);
+        LayoutElement le = placeHolder.AddComponent<LayoutElement>();
+        //le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+        //le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
+        //le.flexibleHeight = 0;
+        //le.flexibleWidth = 0;
+
+        placeHolder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+
         parentToReturnTo = this.transform.parent;
+        placeHolderParent = parentToReturnTo;
         this.transform.SetParent(this.transform.parent.parent);
 
         GetComponent<CanvasGroup>().blocksRaycasts = false;
@@ -34,12 +49,41 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     public void OnDrag(PointerEventData eventData)
     {
         this.transform.position = eventData.position;
+
+        if (placeHolder.transform.parent != placeHolderParent)
+        {
+            placeHolder.transform.SetParent(placeHolderParent);
+        }
+
+        int newSiblingIndex = placeHolderParent.childCount;
+
+        for (int i = 0; i < placeHolderParent.childCount; i++)
+        {
+            //ToDo Update this to handle Vertical Zones
+            if (this.transform.position.x < placeHolderParent.GetChild(i).position.x)
+            {
+                newSiblingIndex = i;
+
+                if (placeHolder.transform.GetSiblingIndex() < newSiblingIndex)
+                {
+                    newSiblingIndex--;
+                }
+
+                break;
+            }
+        }
+
+        placeHolder.transform.SetSiblingIndex(newSiblingIndex);
+
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("OnEndDrag");
         this.transform.SetParent(parentToReturnTo);
+        this.transform.SetSiblingIndex(placeHolder.transform.GetSiblingIndex());
         GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        Destroy(placeHolder);
     }
 }
