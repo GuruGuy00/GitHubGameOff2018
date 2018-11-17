ec33d399d271209970193866bc91c7d6e7f86ddb
mwharris <mharris161@gmail.com>
2018-11-16 23:03:55 -0500

    Removed buttons from cards. Quickly added EnemyController and GameController.

---
+++ GitHubGameOff2018/Assets/EnemyController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class EnemyController : MonoBehaviour
+{
+    public Tilemap groundTilemap;
+    public Vector3 enemyLoc;
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+}
---
+++ GitHubGameOff2018/Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameState gameState;
+    public enum GameState
+    {
+        PlayerTurn,
+        EnemyTurn
+    }
+
+    private Button[] buttons;
+
+    private void Start()
+    {
+        gameState = GameState.PlayerTurn;
+        buttons = FindObjectsOfType<Button>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void StartPlayerTurn()
+    {
+        gameState = GameState.PlayerTurn;
+    }
+
+    public void StartEnemyTurn()
+    {
+        gameState = GameState.EnemyTurn;
+    }
+
+}
