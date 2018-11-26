bfee2ffbc776aa173de6b804445a79a8b230e715
Ken Darling <GuruGuy00@Gmail.com>
2018-11-26 13:58:28 -0500

    Various Updates
    Updated the Cards in the main scene to use the updated one, added some scene management (start --> to game) Added 2 Character sprites

---
+++ GitHubGameOff2018/Assets/SceneLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+        //FindObjectOfType<GameStatus>().ResetGame();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadOptionsScene()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void LoadLevelSelectScene()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+    public void LoadGameOverScene()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+}
