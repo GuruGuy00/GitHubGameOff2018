1493a9cdc474e0cc226c016d7eabe05221872633
Ken Darling <GuruGuy00@Gmail.com>
2018-12-07 15:00:55 -0500

    World Map 2
    More work on the world map

---
+++ GitHubGameOff2018/Assets/LevelMarker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMarker : MonoBehaviour {
+
+    public string levelName;
+
+    public int totalObjectives;
+    public int compleatedObjectives;
+
+    public Sprite[] levelIndicator = new Sprite[3];
+
+    public string LevelToLoad;
+
+    //OnLoad - 
+    // If compleated Objectives = 0 Use levelIndicator 0
+    // IF compleated Objectives = total objectives Use levelIndicator 2
+    // ELSE use levelIndicator 1
+
+    //When Player lands on this object load the scene to play or splash screen to show level detail?
+    //Do we do a onCollision enter, to tigger the load level? or something else
+
+    // Use this for initialization
+    private void Awake()
+    {
+        //Load for save data?
+    }
+
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Load The level
+        Debug.Log("OnTriggerEnter2D - Triggered" + levelName);
+    }
+
+}
---
+++ GitHubGameOff2018/Assets/PlayerControllerWorld.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.transform.position = this.transform.position + Vector3.up;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.transform.position = this.transform.position + Vector3.right;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            this.transform.position = this.transform.position + Vector3.down;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.transform.position = this.transform.position + Vector3.left;
+        }
+
+    }
+}
