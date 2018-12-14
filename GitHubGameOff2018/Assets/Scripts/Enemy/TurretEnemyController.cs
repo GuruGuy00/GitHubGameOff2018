3576268e553a9692d6a0e894b7ec8d7af9ba9e4d
mwharris <mharris161@gmail.com>
2018-12-13 22:42:28 -0500

    Tweaked the player/enemy movement code. Stubbed out a new Turret enemy script.

---
+++ GitHubGameOff2018/Assets/Scripts/Enemy/TurretEnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretEnemyController : IEnemyController
+{
+    private EnemyManager enemyManager;
+
+    void Start()
+    {
+        enemyManager = GameObject.FindObjectOfType<EnemyManager>();
+        tileUtils = TileUtils.Instance;
+        worldLoc = Vector3Int.CeilToInt(transform.position);
+        tileLoc = tileUtils.GetCellPos(tileUtils.groundTilemap, transform.position);
+    }
+
+    public override bool DoEnemyTurn()
+    {
+        return true;
+    }
+
+    public override bool DoEnemyAction()
+    {
+        return true;
+    }
+}
